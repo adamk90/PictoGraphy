9d744a99c10c5c127d45ca577caa23c6cc9436ee
Kubriczky Ádám <kubriczky.adam@gmail.com>
2020-11-25 18:04:06 +0100

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-
-const server = app.listen(3000, function() {
-	console.log("Server listening at :3000");
-});
