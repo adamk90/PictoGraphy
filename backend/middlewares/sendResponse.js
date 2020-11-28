34261de68a57cf36f506368f1201ed02e983947e
Kubriczky Ádám <kubriczky.adam@gmail.com>
2020-11-28 06:03:39 +0100

    Removed body-parser

--- backend/middlewares/sendResponse.js
+++ backend/middlewares/sendResponse.js
@@ -1,7 +1,7 @@
 module.exports = function () {
 	return async function(req, res, next) {
 		if (res.data) {
-			res.json(res.data);
+			return res.json(res.data);
 		}
 		return res.status(400).end();
 	};
