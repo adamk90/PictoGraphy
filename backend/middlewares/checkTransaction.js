c0992f54364c2144c1c773ad7e577199003d52a7
Kubriczky Ádám <kubriczky.adam@gmail.com>
2020-11-29 23:46:12 +0100

    Fix transaction check

--- backend/middlewares/checkTransaction.js
+++ backend/middlewares/checkTransaction.js
@@ -2,6 +2,9 @@ module.exports = function (objectRepository, shouldFind) {
     return async function(req, res, next) {
         if (res.locals.user && res.locals.caff) {
             try {
+                if (res.locals.caff._owner._id && res.locals.user._id.toString() === res.locals.caff._owner._id.toString()) {
+                    return next();
+                }
                 let transaction = await objectRepository.Transaction.findOne({
                     '_product': res.locals.caff._id,
                     '_customer': res.locals.user._id
